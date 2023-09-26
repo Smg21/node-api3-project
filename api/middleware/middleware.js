81b1644cb51c662aabda3e8215ebb74bc8c79108
Sabrina <Smg21022@gmail.com>
2023-09-25 18:26:13 -0700

    all tests passing

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,35 @@
+const Users = require('../users/users-model')
+const Posts = require('../posts/posts-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const date = new Date(Date.now())
+  console.log(req.method, req.originalUrl, date.toString())
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  let user = await Users.getById(req.params.id);
+  if(!user) return res.status(404).json({ message: "user not found" });
+  req.user = user;
+  next();
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if( typeof req.body.name !== 'string' || req.body.name.trim() === '' ) return res.status(400).json({ message: "missing required name field" });
+  req.userName = { name: req.body.name.trim()};
+  console.log(req)
+  next();
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if(!req.body.text) return res.status(400).json({ message: "missing required text field" });
+  req.post = req.body.text;
+  next();
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,147 +1,64 @@
 const express = require('express');
 
-const Users = require('./userDb.js');
-const Posts = require('../posts/postDb.js');
+const Users = require('./users-model');
+const Posts = require('../posts/posts-model');
+const { validateUserId, validateUser, validatePost} = require('../middleware/middleware')
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+
+  Users.get().then(urs => {
+    res.json(urs);
+  })
+});
+
+router.get('/:id', validateUserId, (req, res) => {
+
+    res.json(req.user);
+});
+
 router.post('/', validateUser, (req, res) => {
-  
-  Users.insert(req.body)
-    .then(user => {
-      res.status(201).json(user);
-    })
-    .catch(error => {
-      
-      console.log('POST /api/users Error', error);
 
-      res.status(500).json({ error: 'We ran into an error creating the user' });
-    });
-});
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
-  const postData = { ...req.body, user_id: req.params.id };
-
-  Posts.insert(postData)
-    .then(post => {
-      res.status(201).json(post);
-    })
-    .catch(error => {
-      
-      console.log('POST /api/users/:id/posts Error', error);
-
-      res.status(500).json({ error: 'We ran into an error creating the post' });
-    });
-});
-
-router.get('/', (req, res) => {
-  Users.get()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(error => {
-      
-      console.log('GET /api/users Error', error);
-
-      res
-        .status(500)
-        .json({ error: 'We ran into an error retrieving the users' });
-    });
-});
-
-router.get('/', (req, res) => {
-  Users.get()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(error => {
-      console.log('GET /api/users Error', error);
-
-      res
-        .status(500)
-        .json({ error: 'We ran into an error retrieving the users' });
-    });
-});
-
-router.get('/:id/posts', validateUserId, (req, res) => {
-  Users.getUserPosts(req.params.id)
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-    .catch(error => {
-      console.log('GET /api/user/:id/posts Error', error);
-
-      res
-        .status(500)
-        .json({ error: "We ran into an error retrieving the user's posts" });
-    });
-});
-
-router.delete('/:id', validateUserId, (req, res) => {
-  Users.remove(req.params.id)
-    .then(() => {
-      res.status(200).json({ message: 'user deleted successfully' });
-    })
-    .catch(error => {
-      console.log('DELETE /api/user/:id Error', error);
-
-      res.status(500).json({ error: 'We ran into an error removing the user' });
-    });
+  Users.insert(req.userName).then(usr => {
+    res.json(usr);
+  }).catch((err) => {
+    res.send(err)
+  })
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
-  Users.update(req.params.id, req.body)
-    .then(() => {
-      res.status(200).json({ message: 'User updated successfully' });
-    })
-    .catch(error => {
-      console.log('PUT /api/user/:id Error', error);
 
-      res.status(500).json({ error: 'We ran into an error removing the user' });
-    });
+Users.update(req.params.id, req.userName).then(change => {
+  console.log(change);
+  res.json(change);
+}).catch(err => res.send(err));
 });
 
+router.delete('/:id', validateUserId, (req, res) => {
 
+  Users.remove(req.user.id).then(del => {
+    res.json(req.user);
+  }).catch(err => res.send(err))
+});
 
-function validateUserId(req, res, next) {
-  Users.getById(req.params.id)
-    .then(user => {
-      if (user) {
-        req.user = user;
+router.get('/:id/posts', validateUserId, (req, res) => {
 
-        next();
-      } else {
-        res.status(400).json({ message: 'Invalid user id' });
-      }
-    })
-    .catch(error => {
-      
-      console.log('validateUserId() Error', error);
-
-      res
-        .status(500)
-        .json({ error: 'We ran into an error searching for the user' });
+  Posts.get().then(posts => {
+    const postById = posts.filter(post => {
+      if(post.user_id === req.user.id) return post;
     });
-}
+    res.json(postById)
+  }).catch(err => res.send(err))
+});
 
-function validateUser(req, res, next) {
-  if (!req.body || Object.keys(req.body).length === 0) {
-    res.status(400).json({ message: 'missing user data' });
-  } else if (!req.body.name) {
-    res.status(400).json({ message: 'missing required name field' });
-  } else {
-    next();
-  }
-}
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 
-function validatePost(req, res, next) {
-  if (!req.body || Object.keys(req.body).length === 0) {
-    res.status(400).json({ message: 'missing post data' });
-  } else if (!req.body.text) {
-    res.status(400).json({ message: 'missing required text field' });
-  } else {
-    next();
-  }
-}
+  Posts.insert({text: req.post, user_id: req.user.id }).then(post => {
+    res.json(post);
+  }).catch(err => res.send(err))
+});
+
 
 module.exports = router;
